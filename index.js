6206fa2e16e4f84571374e80d2bddf73f217a326
ona tatsuya <tatsuya@wlan3231.u-aizu.ac.jp>
2017-07-11 11:54:20 +0900

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+$(".btn").click(function(){
+	//alert($(this));
+	//$("body").toggleClass("active");
+	$(".bar1").toggleClass("active");  
+	$(".bar2").toggleClass("active"); 
+	$(".bar3").toggleClass("active"); 
+    });
